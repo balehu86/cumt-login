7f874863aee55411a16d3f9e4b8a127941921793
achieve <44516835+chieve-bale@users.noreply.github.com>
2023-06-26 13:05:30 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#shutdown the openclash
-/etc/init.d/openclash stop
-
 #login method
 login(){
         curl -s -X GET 'http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=02210000%40cmcc&user_password=123456789&wlan_user_ip=0.0.0.0&wlan_user_mac=00:00:00:00:00:00'
@@ -25,12 +22,8 @@ fi
 
 #get the time
 time=$(date '+%y-%m-%d %H:%M:%S')
-#send message to telegram
-#curl -s "https://api.telegram.org/bot5329000000:AAH8dHkIRpwPxxhSfCiGWoCikpMka000000/getupdates"
+
 #print log to logfile
 echo '认证成功   '$time > /tmp/log/login.log
 echo '认证成功   '$time >> /root/login.sh.log
 #echo "认证成功"
-
-#boot the openclash
-/etc/init.d/openclash start
