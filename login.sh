6f5795d8d869c6fa70429a8e9568724b1fdd4430
achieve <44516835+chieve-bale@users.noreply.github.com>
2024-05-24 20:17:28 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-#login method
-login(){
-        curl -s -X GET 'http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=02210000%40cmcc&user_password=123456789&wlan_user_ip=0.0.0.0&wlan_user_mac=00:00:00:00:00:00'
-}
-
-#judge the net work well or not
+time=$(date '+%y-%m-%d %H:%M:%S')
 destination_ip=www.runoob.com
 
-if ping -c1 $destination_ip &>/dev/null 
-then  
-netstatu=online
-else 
-netstatu=offline 
-fi
- 
-if [ $netstatu == 'offline' ]
+##login method
+login(){
+        curl -s -X GET 'http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=02210765%40cmcc&user_password=OhMaU286&wlan_user_ip=10.4.248.62&wlan_user_mac=70:E2:4C:68:3E:A9'
+}
+
+if ping -c1 $destination_ip &>/dev/null
 then
+        echo "网络正常"
+        curl -X GET "https://api.telegram.org/bot5329390000:AAH8dHkIRpwPxxhSfCiGWoCikpMkaW00000/sendMessage?chat_id=1217560000&text=网络正常"
+else
+        #/etc/init.d/openclash stop
+        echo "无网络，尝试登录"
         login
+        if ping -c1 $destination_ip &>/dev/null
+        then
+                echo "认证成功"
+                curl -X GET "https://api.telegram.org/bot5329390000:AAH8dHkIRpwPxxhSfCiGWoCikpMkaW00000/sendMessage?chat_id=1217560000&text=已成功登录"
+        fi
+        #/etc/init.d/openclash start
 fi
 
-#get the time
-time=$(date '+%y-%m-%d %H:%M:%S')
-
-#print log to logfile
-echo '认证成功   '$time > /tmp/log/login.log
-echo '认证成功   '$time >> /root/login.sh.log
-#echo "认证成功"
+##print log to logfile
+echo '认证成功   '$time > /tmp/log/school.sh.log
+echo '认证成功   '$time >> /root/school.sh.log
