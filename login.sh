5b0b01ee7961f515f8b5d91800983ac7f9c865c8
achieve <44516835+chieve-bale@users.noreply.github.com>
2024-07-12 17:26:35 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -5,25 +5,28 @@ destination_ip=www.runoob.com
 
 ##login method
 login(){
-        curl -s -X GET 'http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=02210765%40cmcc&user_password=OhMaU286&wlan_user_ip=10.4.248.62&wlan_user_mac=70:E2:4C:68:3E:A9'
+    curl -X GET 'http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=02210000%40cmcc&user_password=00000000&wlan_user_ip=&wlan_user_mac=00:00:00:00:00:A9'
+}
+send(){
+    echo $1
+    curl -s -X GET "https://api.telegram.org/bot5329391020:AAH8dHkIRpwPxxhSfCiGWoCikpMkaW00000/sendMessage?chat_id=1217500000&text="$1
+    ##print log to logfile
+    echo $1  $time > /tmp/log/school.sh.log
+    echo $1  $time >> /root/school.sh.log
 }
 
 if ping -c1 $destination_ip &>/dev/null
 then
-        echo "网络正常"
-        curl -X GET "https://api.telegram.org/bot5329390000:AAH8dHkIRpwPxxhSfCiGWoCikpMkaW00000/sendMessage?chat_id=1217560000&text=网络正常"
+    send "网络正常"
 else
-        #/etc/init.d/openclash stop
-        echo "无网络，尝试登录"
-        login
-        if ping -c1 $destination_ip &>/dev/null
-        then
-                echo "认证成功"
-                curl -X GET "https://api.telegram.org/bot5329390000:AAH8dHkIRpwPxxhSfCiGWoCikpMkaW00000/sendMessage?chat_id=1217560000&text=已成功登录"
-        fi
-        #/etc/init.d/openclash start
+    #/etc/init.d/openclash stop
+    #send "尝试认证"
+    echo "尝试认证"
+    login
+    if ping -c1 $destination_ip &>/dev/null
+    then
+        send "认证成功"
+    else
+        send "认证失败"
+    fi
 fi
-
-##print log to logfile
-echo '认证成功   '$time > /tmp/log/school.sh.log
-echo '认证成功   '$time >> /root/school.sh.log
