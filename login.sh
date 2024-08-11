46e63deb95a1fe544cd2692522280a9bf922ddbd
achieve <44516835+chieve-bale@users.noreply.github.com>
2024-08-11 20:19:19 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -2,31 +2,44 @@
 
 time=$(date '+%y-%m-%d %H:%M:%S')
 destination_ip=www.runoob.com
+user_account='02210000%40cmcc'
+user_password='00000000'
 
-##login method
+########login method########
 login(){
-    curl -X GET 'http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=02210000%40cmcc&user_password=00000000&wlan_user_ip=&wlan_user_mac=00:00:00:00:00:A9'
+    if [ $1 != '网络正常' ]
+    then
+        send $1 0
+        resault=$(curl -X GET 'http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=02210000%40cmcc&user_password=00000000&wlan_user_ip=&wlan_user_mac=70:E2:4C:68:3E:A9')
+        if [ $resault == '({"result":"1","msg":"认证成功"})' ]
+        then
+            send '网络异常，已认证成功'
+        else
+            send '网络异常，认证失败' 0
+        fi
+    fi
 }
+
+########send message to telegram bot and echo into log file########
 send(){
-    echo $1
-    curl -s -X GET "https://api.telegram.org/bot5329391020:AAH8dHkIRpwPxxhSfCiGWoCikpMkaW00000/sendMessage?chat_id=1217500000&text="$1
+    echo $1 $2
+    if [ "$2" != '0' ]
+    then
+        curl -s -X GET "https://api.telegram.org/bot5329000000:AAH8dHkIRpwPxxhSfCiGWoCikpMkaW00000/sendMessage?chat_id=1217560000&text="$1 > /dev/null 2>&1
+    fi
     ##print log to logfile
-    echo $1  $time > /tmp/log/school.sh.log
-    echo $1  $time >> /root/school.sh.log
+    echo $time  $1 > /tmp/log/school.sh.log
+    echo $time  $1 >> /root/school.sh.log
 }
 
 if ping -c1 $destination_ip &>/dev/null
 then
-    send "网络正常"
+    send "网络正常！"
 else
     #/etc/init.d/openclash stop
-    #send "尝试认证"
-    echo "尝试认证"
-    login
-    if ping -c1 $destination_ip &>/dev/null
+    login "网络异常，尝试认证"
+    if ![ping -c1 $destination_ip &>/dev/null]
     then
-        send "认证成功"
-    else
-        send "认证失败"
+        login "认证失败" 0
     fi
 fi
