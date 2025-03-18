8a2f0b7112edde387576c4f47ab7fec6430eb154
bale hu <44516835+balehu86@users.noreply.github.com>
2025-03-18 18:18:39 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -10,7 +10,7 @@ login(){
     if [ $1 != '网络正常' ]
     then
         send $1 0
-        resault=$(curl -X GET 'http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=02210000%40cmcc&user_password=00000000&wlan_user_ip=&wlan_user_mac=70:E2:4C:68:3E:A9')
+        resault=$(curl -X GET 'http://10.2.5.251:801/eportal/?c=Portal&a=login&login_method=1&user_account=022100000%40cmcc&user_password=00000000&wlan_user_ip=&wlan_user_mac=70:E2:4C:68:3E:00')
         if [ $resault == '({"result":"1","msg":"认证成功"})' ]
         then
             send '网络异常，已认证成功'
@@ -25,7 +25,7 @@ send(){
     echo $1 $2
     if [ "$2" != '0' ]
     then
-        curl -s -X GET "https://api.telegram.org/bot5329000000:AAH8dHkIRpwPxxhSfCiGWoCikpMkaW00000/sendMessage?chat_id=1217560000&text="$1 > /dev/null 2>&1
+        curl -s -X GET "https://api.telegram.org/bot5329390000:AAH8dHkIRpwPxxhSfCiGWoCikpMkaW30000/sendMessage?chat_id=1217566000&text="$1 > /dev/null 2>&1
     fi
     ##print log to logfile
     echo $time  $1 > /tmp/log/school.sh.log
@@ -34,7 +34,7 @@ send(){
 
 if ping -c1 $destination_ip &>/dev/null
 then
-    send "网络正常！"
+    send "网络正常！" 0
 else
     #/etc/init.d/openclash stop
     login "网络异常，尝试认证"
